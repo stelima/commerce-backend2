184cbda54f34b437ac5f050763177789885d3aa4
stelima <stefanie.araujo@quintoandar.com.br>
2023-06-28 01:32:15 -0300

    wip

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import handlebars from 'express-handlebars';
 import viewsRouter from './routes/views.router.js';
 import { Server } from 'socket.io';
-const teste = 'products.json';
-console.log('teste', teste)
+import fs from 'fs';
+
 const app = express();
 const server = app.listen(8080, () => console.log('Servidor rodando na porta 8080'));
 
@@ -16,13 +16,25 @@ app.set('views', viewFolder);
 app.set('view engine', 'handlebars');
 app.use('/', viewsRouter);
 
-socketServer.on('connection', socket => {
+const productsData = fs.readFileSync('products.json', 'utf-8');
+const products = JSON.parse(productsData);
+
+app.get('/', (req, res) => {
+    res.render('home', { products });
+  });
+
+  socketServer.on('connection', socket => {
     console.log('Novo produto');
 
     socket.on('product', data => {
-        console.log('Produto salvo', data);
+      console.log(`Produto cadastrado ${data}`);
+      const id = products.length > 0 ? Math.max(...products.map(product => product.id)) + 1 : 1;
+      const dataList = { id, ...data }
+      products.push(dataList);
+      fs.writeFileSync('products.json', JSON.stringify(products, null, 2), 'utf-8');
+      socketServer.emit('product', products);
     });
-});
+  });
 
 socketServer.on('disconnect', () => {
     console.log('Usuario desconectao');
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -10,5 +10,12 @@ router.get('/realTimeProducts', (req, res) => {
     res.render('realTimeProducts');
 });
 
+router.post('/save-product', (req, res) => {
+    const productData = req.body;
+
+    socketServer.emit('product', productData);
+
+    res.sendStatus(200);
+});
 
 export default router;
